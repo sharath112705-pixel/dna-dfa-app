98f475cab0db9a8b8bb778e6ce6f1c41a1d1154e
sharath112705-pixel <sharath112705@gmail.com>
2025-11-22 10:41:56 +0530

    Create dfa_graph.py

---
+++ dfa_graph.py
@@ -0,0 +1,24 @@
+def plot_dna_dfa():
+    graph = f"""
+    digraph {
+        rankdir=LR
+        node [shape=circle, style=filled, fontcolor=black, fontsize=16]
+
+        q0 [fillcolor="lightgray", label="q0"]
+        q1 [fillcolor="lightblue", label="q1"]
+        q2 [fillcolor="orange", label="q2"]
+        q3 [fillcolor="lightgreen", label="q3 (Accept)"]
+
+        q0 -> q1 [label="A"]
+        q1 -> q2 [label="T"]
+        q2 -> q3 [label="G"]
+        q3 -> q1 [label="A"]
+
+        # Other transitions
+        q0 -> q0 [label="T,G,C"]
+        q1 -> q0 [label="A,C,G"]
+        q2 -> q0 [label="A,T,C"]
+        q3 -> q0 [label="T,G,C"]
+    }
+    """
+    return graph
